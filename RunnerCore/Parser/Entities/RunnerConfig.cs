6b636a9853d35c999f039dc32fbe5a637af14167
DarkLord <dfhrhfan12@yandex.ru>
2022-07-19 22:33:40 +0400

    Add parser to RunnerCore lib

---
+++ RunnerCore/Parser/Entities/RunnerConfig.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RunnerCore.Parser.Entities
+{
+    public class RunnerConfig
+    {
+        public string HelloMessage { get; set; } = string.Empty;
+        public IReadOnlyList<string> BeforeScriptLines { get; set; } = Array.Empty<string>();
+        public IReadOnlyList<string> AfterScriptLines { get; set; } = Array.Empty<string>();
+        public IReadOnlyList<ScriptInfo> Content { get; set; } = Array.Empty<ScriptInfo>();
+    }
+}
