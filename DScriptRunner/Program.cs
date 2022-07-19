e9959d5a1b8fbfe353453cbd88595bf148b08d5c
DarkLord <dfhrhfan12@yandex.ru>
2022-07-19 11:57:50 +0400

    Add Runner form

--- DScriptRunner/Program.cs
+++ DScriptRunner/Program.cs
@@ -17,7 +17,8 @@ namespace DScriptRunner
             Application.SetHighDpiMode(HighDpiMode.SystemAware);
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            var form = new RunnerForm();
+            Application.Run(form);
         }
     }
 }
---
+++ DScriptRunner/RunnerForm.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+using System.Windows.Forms;
+
+namespace DScriptRunner
+{
+    class RunnerForm : Form
+    {
+        private NotifyIcon appIcon;
+
+        public RunnerForm()
+        {
+            var menuContainer = PrepareMenu();
+            appIcon = new NotifyIcon()
+            {
+                Icon = RunnerResources.AppIcon,
+                Visible = true,
+                ContextMenuStrip = menuContainer,
+            };
+        }
+
+        private ContextMenuStrip PrepareMenu()
+        {
+            var menuItem1 = new ToolStripMenuItem("Temp1");
+
+            var menuItem2 = new ToolStripMenuItem("Temp2");
+            menuItem1.DropDownItems.Add(menuItem2);
+
+            var item = menuItem2.DropDownItems.Add("Exit");
+            item.Click += Exit;
+
+            var contextMenu = new ContextMenuStrip();
+            contextMenu.Items.Add(menuItem1); 
+            return contextMenu;
+        }
+
+        private void Exit(object? sender, EventArgs e)
+        {
+            appIcon.Visible = false;
+            Application.Exit();
+        }
+    }
+}
