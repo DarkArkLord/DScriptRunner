bc6ffb95b48197fba954a96107122166844053a0
DarkLord <dfhrhfan12@yandex.ru>
2022-07-19 13:51:15 +0400

    Add nodes name

---
+++ DScriptRunner/ConfigNodes.cs
@@ -0,0 +1,16 @@
+﻿namespace DScriptRunner
+{
+    static class ConfigNodes
+    {
+        public const string Main = "Config";
+
+        public const string Global = "Global";
+        public const string GlobalHello = "Hello";
+        public const string GlobalBefore = "Before";
+        public const string GlobalAfter = "After";
+
+        public const string Scripts = "Scripts";
+        public const string ScriptsGroup = "Group";
+        public const string ScriptsScript = "Script";
+    }
+}
