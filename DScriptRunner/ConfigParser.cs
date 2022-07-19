ba40736d94bf5b6c2f8d4a42db7bd2f431584ebb
DarkLord <dfhrhfan12@yandex.ru>
2022-07-19 14:17:44 +0400

    Add parser class

---
+++ DScriptRunner/ConfigParser.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Xml.Linq;
+
+namespace DScriptRunner
+{
+    static class ConfigParser
+    {
+        public static XDocument ReadXml(string path)
+        {
+            var file = File.ReadAllText(path);
+            var xDocument = XDocument.Parse(file);
+            return xDocument;
+        }
+    }
+}
