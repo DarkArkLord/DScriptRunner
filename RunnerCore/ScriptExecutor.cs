d6c6503a512e393ef5313c29b7a9548dacda66cb
DarkLord <dfhrhfan12@yandex.ru>
2022-07-23 00:01:47 +0400

    Add many files

--- RunnerCore/ScriptExecutor.cs
+++ RunnerCore/ScriptExecutor.cs
@@ -9,6 +9,8 @@ namespace RunnerCore
 {
     public class ScriptExecutor
     {
+        private static readonly List<bool> usedFiles = new List<bool>();
+
         private readonly ScriptLines currentScript;
         private readonly RunnerConfig config;
 
@@ -26,12 +28,14 @@ namespace RunnerCore
         public void Execute(object sender, EventArgs e)
         {
             var script = ConcatScript();
-            var fileName = GiveFileName();
+            var fileIndex = GetFileIndex();
+            var fileName = GetFileName(fileIndex);
             var path = Path.Combine(Directory.GetCurrentDirectory(), fileName);
             File.WriteAllLines(path, script);
             var process = Process.Start("powershell", path);
             process.WaitForExit();
             File.Delete(path);
+            FreeFileIndex(fileIndex);
         }
 
         private IReadOnlyList<string> ConcatScript()
@@ -52,9 +56,36 @@ namespace RunnerCore
             return script;
         }
 
-        private string GiveFileName()
+        private int GetFileIndex()
         {
-            return "temp.ps1";
+            lock (usedFiles)
+            {
+                for (int i = 0; i < usedFiles.Count; i++)
+                {
+                    if (!usedFiles[i])
+                    {
+                        usedFiles[i] = true;
+                        return i;
+                    }
+                }
+
+                var newIndex = usedFiles.Count;
+                usedFiles.Add(true);
+                return newIndex;
+            }
+        }
+
+        private string GetFileName(int index)
+        {
+            return $"temp{index}.ps1";
+        }
+
+        private void FreeFileIndex(int index)
+        {
+            lock (usedFiles)
+            {
+                usedFiles[index] = false;
+            }
         }
     }
 }
