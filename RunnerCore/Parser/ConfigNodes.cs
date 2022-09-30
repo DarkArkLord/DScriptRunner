a19303b9b65356b4b940df0939425d35e821727b
DarkLord <dfhrhfan12@yandex.ru>
2022-09-30 22:50:32 +0400

    Upd config

--- RunnerCore/Parser/ConfigNodes.cs
+++ RunnerCore/Parser/ConfigNodes.cs
@@ -6,13 +6,8 @@
 
         public const string Hello = "Hello";
 
-        public const string Global = "Global";
-        public const string GlobalBefore = "Before";
-        public const string GlobalAfter = "After";
-
         public const string Environments = "Environments";
         public const string Environment = "Environment";
-        // Add global
         public const string EnvironmentBefore = "Before";
         public const string EnvironmentAfter = "After";
 
