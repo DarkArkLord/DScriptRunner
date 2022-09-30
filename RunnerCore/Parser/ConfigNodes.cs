96d979e3b0c423a8e515514186dd8b7b4b20be50
DarkLord <dfhrhfan12@yandex.ru>
2022-09-30 18:39:23 +0400

    Add env names

--- RunnerCore/Parser/ConfigNodes.cs
+++ RunnerCore/Parser/ConfigNodes.cs
@@ -10,10 +10,17 @@
         public const string GlobalBefore = "Before";
         public const string GlobalAfter = "After";
 
+        public const string Environments = "Environments";
+        public const string Environment = "Environment";
+        // Add global
+        public const string EnvironmentBefore = "Before";
+        public const string EnvironmentAfter = "After";
+
         public const string Scripts = "Scripts";
         public const string ScriptsGroup = "Group";
         public const string ScriptsScript = "Script";
 
         public const string AttributeName = "name";
+        public const string AttributeEnvironment = "env";
     }
 }
