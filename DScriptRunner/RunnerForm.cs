a51a51e95fa66aaa58649d2a3b9d1b6f8ca87c3b
DarkLord <dfhrhfan12@yandex.ru>
2022-09-30 23:08:44 +0400

    Upd version

--- DScriptRunner/RunnerForm.cs
+++ DScriptRunner/RunnerForm.cs
@@ -96,7 +96,7 @@ namespace DScriptRunner
                        "Разработчик: Алексей Петров aka DarkNessLord\n" +
                        "https://github.com/ShadowOfFallenLord/DScriptRunner \n" +
                        "2022 год \n" +
-                       "Версия 1.0.2";
+                       "Версия 1.1.0";
             MessageBox.Show(text, "О программе");
         }
 
