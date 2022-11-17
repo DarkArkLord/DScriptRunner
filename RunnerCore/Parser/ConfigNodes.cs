a72cc1e8ae75d7665e5c6a446bf870b67f75d53c
DarkLord <dfhrhfan12@yandex.ru>
2022-11-18 00:55:04 +0400

    Upd nodes

--- RunnerCore/Parser/ConfigNodes.cs
+++ RunnerCore/Parser/ConfigNodes.cs
@@ -6,17 +6,15 @@
 
         public const string Hello = "Hello";
 
-        public const string Environments = "Environments";
-        public const string Environment = "Environment";
-        public const string EnvironmentBefore = "Before";
-        public const string EnvironmentAfter = "After";
+        public const string ScriptElements = "ScriptElements";
+        public const string ScriptElement = "ScriptElement";
 
         public const string Scripts = "Scripts";
         public const string ScriptsGroup = "Group";
         public const string ScriptsScript = "Script";
+        public const string ScriptText = "ScriptText";
 
         public const string AttributeName = "name";
-        public const string AttributeEnvironment = "env";
         public const string AttributeHidden = "hidden";
     }
 }
