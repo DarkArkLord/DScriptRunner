f84719bbdb5b0ed23ec0ca7afef24cdb9123a673
DarkLord <dfhrhfan12@yandex.ru>
2023-10-06 21:42:24 +0400

    Add comments

--- RunnerCore/Entities/RunnerConfig.cs
+++ RunnerCore/Entities/RunnerConfig.cs
@@ -8,9 +8,13 @@ namespace RunnerCore.Entities
     /// </summary>
     public class RunnerConfig
     {
+        // Приветственное сообщение
         public string HelloMessage { get; set; } = string.Empty;
+        // Скриптовые вставки
         public Dictionary<string, IReadOnlyList<string>> ScriptInserts { get; set; } = new Dictionary<string, IReadOnlyList<string>>();
+        // Среды
         public Dictionary<string, RunnerEnvironment> Environments { get; set; } = new Dictionary<string, RunnerEnvironment>();
+        // Набор групп и скриптов
         public IReadOnlyList<ScriptInfo> Content { get; set; } = Array.Empty<ScriptInfo>();
     }
 
@@ -19,7 +23,9 @@ namespace RunnerCore.Entities
     /// </summary>
     public class RunnerEnvironment
     {
+        // Строки, добавляемые перед скриптом
         public IReadOnlyList<string> BeforeLines { get; set; }
+        // Строки, добавляемые после скрипта
         public IReadOnlyList<string> AfterLines { get; set; }
     }
 }
--- RunnerCore/Entities/ScriptInfo.cs
+++ RunnerCore/Entities/ScriptInfo.cs
@@ -8,9 +8,13 @@ namespace RunnerCore.Entities
     /// </summary>
     public abstract class ScriptInfo
     {
+        // Показывает, скрипт это или группа
         public abstract bool IsScript { get; }
+        // Название
         public string Title { get; protected set; }
+        // Вложенные конфигурации для группы
         public IReadOnlyList<ScriptInfo> Content { get; protected set; } = Array.Empty<ScriptInfo>();
+        // Строки скрипта
         public IReadOnlyList<string> ScriptLines { get; protected set; } = Array.Empty<string>();
 
         protected ScriptInfo(string title)
